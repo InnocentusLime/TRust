6f5a3a8f7dbc476c1bdb35309f8f82fd6477172e
InnocentusLime <innocentuslime.help@gmail.com>
2024-11-11 16:09:10 +0000

    Add basic ui funcs

--- bin/main.ml
+++ bin/main.ml
@@ -1,6 +1,4 @@
 let usage_msg = "trust [-v] [-q] [-Q] [file1] [file2] ..."
-let verbose = ref false
-let quiet = ref false
 let quit_on_done = ref false
 let modules = ref []
 
@@ -8,10 +6,13 @@ let anon_fun filename =
   modules := filename :: !modules
 
 let speclist =
-  [("-v", Arg.Set verbose, "Print debug information");
-   ("-q", Arg.Set quiet, "Quiet mode");
+  [("-v", Arg.Set Ui.verbose, "Print debug information");
+   ("-q", Arg.Set Ui.quiet, "Quiet mode");
    ("-Q", Arg.Set quit_on_done, "Quit when done executing")]
 
 let () =
   Arg.parse speclist anon_fun usage_msg;
+  Ui.debug "Verbose: %B\nQuit-on-done:%B\nModules:\n" !Ui.verbose !Ui.quiet;
+  List.fold_right (fun x () -> Ui.debug "* %s\n" x) !modules ();
+  Ui.info "Welcome to TRust. Type `help.` for available commands\n";
   Top.run (Top.make_empty_ctx ()) |> fun _ -> ()
